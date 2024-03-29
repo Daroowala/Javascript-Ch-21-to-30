42dc424cf0dd4a76ae4a400d06f6b22b33ba87e9
my name <blueshaperd3@gmail.com>
2024-03-29 17:15:27 +0500

    not done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var city = prompt("Enter your city name")
-var arr = ["Karachi", "Islamabad", "Pishawer"]
-for (var i = 0; i<arr.length; i++){
+city = city.toLowerCase()
+var arr = ["karachi", "islamabad", "lahore"]
+for (var i = 0; i < arr.length; i++){
     if(arr[i] === city){
-        alert("City has found")
-    }else{alert("City is not found")}
+        alert("city is found")
+        break
+    }
+    else{alert("city is not found")}
 }

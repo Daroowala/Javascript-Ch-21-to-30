5d021bfa00ba21c64ac9df570c8549aec762337f
my name <blueshaperd3@gmail.com>
2024-03-29 17:34:19 +0500

    lower and upper case all done

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+// Lower and Uppercase changing
+// var city = prompt("Enter your city name")
+// city = city.toLowerCase()
+// var arr = ["karachi", "islamabad", "lahore"]
+// var found = false;
+// for (var i = 0; i < arr.length; i++){
+//     if(arr[i] === city){
+//         alert("city is found")
+//         found = true;
+//         break
+//     }
+// }
+// if (!found) {
+//     alert("city is not found")
+// }
+
 var city = prompt("Enter your city name")
-city = city.toLowerCase()
-var arr = ["karachi", "islamabad", "lahore"]
+city = city.toUpperCase()
+var arr = ["KARACHI", "ISLAMABAD", "LAHORE"]
 var found = false;
 for (var i = 0; i < arr.length; i++){
     if(arr[i] === city){
